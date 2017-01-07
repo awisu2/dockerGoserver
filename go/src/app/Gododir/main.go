33f3e3186ab2357e51ce6aceddc28bc6be47a235
awisu2 <awisu2@gmail.com>
2017-01-07 12:42:21 +0900

    整理整頓

---
+++ go/src/app/Gododir/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	do "gopkg.in/godo.v2"
+)
+
+func tasks(p *do.Project) {
+	p.Task("default", do.S{"build"}, nil)
+
+	p.Task("build", nil, func(c *do.Context) {
+		c.Run("GOOS=linux GOARCH=amd64 go build", do.M{"$in": "./"})
+	}).Src("**/*.go")
+
+	p.Task("get?", nil, func(c *do.Context) {
+		c.Run("go get", do.M{"$in": "./"})
+	})
+
+	p.Task("server", do.S{"get"}, func(c *do.Context) {
+		c.Start("main.go", do.M{"$in": "./"})
+	}).Src("*.go", "**/*.go").Debounce(3000)
+}
+
+func main() {
+	do.Godo(tasks)
+}
--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE hoge DEFAULT CHARACTER SET utf8;
+CREATE DATABASE foo DEFAULT CHARACTER SET utf8;
 
-use hoge;
+use foo;
 
 CREATE TABLE person (
   id MEDIUMINT NOT NULL AUTO_INCREMENT,
@@ -13,21 +13,3 @@ INSERT INTO person VALUES
 (2, 'second'),
 (3, 'therd'),
 (4, 'fource');
-
-CREATE TABLE scraping (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  url VARCHAR(256) COMMENT '取得対象のURL',
-  pattern_id int COMMENT '取得したデータの解析パターンid(scraping_pattern table)',
-  detail VARCHAR(300) COMMENT 'このスクレイピングの説明',
-  to_id int COMMENT '解析後の処理パターン',
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE scrf_token (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  token VARCHAR(256) NOT NULL UNIQUE COMMENT 'scrf token',
-  enable bool NOT NULL DEFAULT FALSE COMMENT 'enable flag',
-  PRIMARY KEY (id),
-  key token_index(token)
-);
-
